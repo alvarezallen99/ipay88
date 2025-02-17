3847ecf985a9e60ebbd5b1a9e5d182e52abead1b
Allen Alvarez <allen.alvarez@guerilla360.com>
2025-02-17 15:38:00 +0800

    bugfix: Remove unnecessary line of code

--- src/IPay88/Payment/Request.php
+++ src/IPay88/Payment/Request.php
@@ -232,7 +232,7 @@ class Request
      */
     public static function make($merchantKey, $fieldValues)
     {
-        new Request($merchantKey);
+        // new Request($merchantKey);
         RequestForm::render($fieldValues, self::$paymentUrl);
     }
 
